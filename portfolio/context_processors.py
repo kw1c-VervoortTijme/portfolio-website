f4a85e31026213fd9f286dcb60f3d5d75a1f1e27
kw1c-VervoortTijme <tijme.vervoort@edu-kw1c.nl>
2025-05-14 11:30:33 +0200

    changes to show it works

--- portfolio/context_processors.py
+++ portfolio/context_processors.py
@@ -1,15 +1,14 @@
 # portfolio/context_processors.py
-
-from .translations import translate, TRANSLATIONS
+from .translations import TRANSLATIONS
 
 def translation_context(request):
     """
-    Add translation function and available languages to the template context.
+    Add translation context variables to templates.
     """
+    # Get language from session or default to English
     current_language = request.session.get('language', 'en')
     
     return {
-        'translate': lambda key: translate(key, current_language),
         'available_languages': list(TRANSLATIONS.keys()),
         'current_language': current_language,
     }
---
+++ portfolio/templatetags/translation_tags.py
@@ -0,0 +1,25 @@
+from django import template
+from portfolio.translations import translate, TRANSLATIONS
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def t(context, key):
+    """
+    Template tag to translate text keys.
+    Usage: {% t 'home_title' %}
+    """
+    request = context.get('request')
+    if request:
+        language = request.session.get('language', 'en')
+    else:
+        language = 'en'
+    return translate(key, language)
+
+@register.simple_tag
+def get_language_name(code):
+    """
+    Return the name of the language given its code.
+    Usage: {% get_language_name 'en' %}
+    """
+    return code.upper()
